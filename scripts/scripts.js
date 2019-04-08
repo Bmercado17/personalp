85230ec72ab442d7623d5b8f8faef8492e4f071c
Bernardo Sanchez Mercado <bernardosanchezmercado@Bernardos-iMac.local>
2019-04-08 11:11:18 -0700

    second commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,4 @@
-//alert ("im an alert box");
-// $(function(){
-//$("h1").click(function() {
-    //alert("this is a header.");
-//});
-//$("p").click(function(){
-  //  alert("this a paragraph");
-//});
-//$("img").click(function(){
-  //  alert("this is an image");
-//});
-//}); 
+//business logic (back end)
+$(document).ready(function(){
+  
+});
